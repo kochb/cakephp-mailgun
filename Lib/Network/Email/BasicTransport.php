3a38a73601f3d28b6fc89d5d81685477cccdc54c
alan bount <alan@zeroasterisk.com>
2013-11-21 17:48:56 -0500

    updated BasicTransport to act like Curl (processing, error reporting)

--- Lib/Network/Email/BasicTransport.php
+++ Lib/Network/Email/BasicTransport.php
@@ -6,7 +6,7 @@ App::uses('HttpSocket', 'Network/Http');
  * Enables sending of email over mailgun
  *
  * Licensed under The MIT License
- * 
+ *
  * @author Brad Koch <bradkoch2007@gmail.com>
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
@@ -27,16 +27,21 @@ class BasicTransport extends AbstractTransport {
  */
 	public function send(CakeEmail $email) {
         $http = new HttpSocket();
-        
+
         $url = 'https://api.mailgun.net/v2/' . $this->_config['mailgun_domain'] . '/messages';
-        $headers = $email->getHeaders();
-        $data = array_merge(
-            $headers,
+        $post = array();
+        $post_preprocess = array_merge(
+            $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'bcc', 'subject')),
             array(
                 'text' => $email->message(CakeEmail::MESSAGE_TEXT),
                 'html' => $email->message(CakeEmail::MESSAGE_HTML)
             )
         );
+        foreach ($post_preprocess as $k => $v) {
+            if (! empty($v)) {
+                $post[strtolower($k)] = $v;
+            }
+        }
         $request = array(
             'auth' => array(
                 'method' => 'Basic',
@@ -45,10 +50,18 @@ class BasicTransport extends AbstractTransport {
             )
         );
 
-        $http->post($url, $data, $request);
-        
+        $response = $http->post($url, $post, $request);
+        if ($response === false) {
+            throw new SocketException("Mailgun BasicTransport error, no response", 500);
+        }
+
+        $http_status = $response->code;
+        if ($http_status != 200) {
+            throw new SocketException("Mailgun request failed.  Status: $http_status, Response: {$response->body}", 500);
+        }
+
         return array(
-            'headers' => $this->_headersToString($headers, PHP_EOL),
+            'headers' => $this->_headersToString($email->getHeaders(), PHP_EOL),
             'message' => implode(PHP_EOL, $email->message())
         );
     }
